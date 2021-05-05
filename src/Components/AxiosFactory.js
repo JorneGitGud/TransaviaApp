fcc3363464166d9fc2cfa161def5ac99bc38bcb3
Jorne <mr.jorne@gmail.com>
2021-05-05 13:42:30 +0200

    Form creation
    Ticket.js form now functional and semi-styled

---
+++ src/Components/AxiosFactory.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['apikey'] = 'd9bdfa4543364f9cbec8855da5a4d353';
+
+export default axios;
--- src/views/Tickets.js
+++ src/views/Tickets.js
@@ -1,36 +1,93 @@
 import { useContext, useEffect, useState } from "react";
-import axios from 'axios';
+import Select from 'react-select'
+import axios from '../Components/AxiosFactory';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const Tickets = () => {
     //variable
     const [airports, setAirports] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(true);
-    const primaryKey = 'd9bdfa4543364f9cbec8855da5a4d353';
+    const [selectedAirport, setSelectedAirport] = useState('')
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const timeOptions = ([2, 4, 6, 8, 12, 16, 24, 48])
+    const [selectTimeOption, setSelectTimeOptions] = useState([])
+    const [overnights, setOvernights] = useState(0)
+
+
 
     //methods
     useEffect(() => {
-        axios.get(`https://api.transavia.com/v2/airports/`, { headers: { apikey: primaryKey } })
+        getAirports();
+        setTimeVariables();
+
+    }, []);
+
+    const setTimeVariables = () => {
+        const options = timeOptions.map(time => ({
+            "value": time,
+            "label": time + " uur."
+        }))
+        setSelectTimeOptions(options)
+    }
+
+    const getAirports = (() => {
+        axios.get(`https://api.transavia.com/v2/airports/`)
             .then(res => {
-                setAirports(res.data)
-                console.log('fetching data')
+                const data = res.data;
+                const options = data.map(data => ({
+                    "value": data.id,
+                    "label": data.name + ", " + data.country.name
+                }))
+                setAirports(options)
             })
             .then(() => {
-                if (airports.length > 10) {
-                    console.log(airports)
-                } else {
-                    setIsLoaded(false);
-                }
+                if (airports) console.log('fetched data')
             }
             ).catch((e) => {
                 console.log('Something went wrong fetching the data.  ' + e);
             })
-    }, [isLoaded]);
+    })
+
+    
+
+    const handleSelectAirport = (choice) => {
+        setSelectedAirport(choice.value.toString());
+        console.log('selected airport is now: ' + choice.value.toString())
+    }
+    const handleChangeOvernights = (nights) =>{
+        console.log(nights)
+    }
+
 
     //template
     return (
         <div className="tickets">
             <h1>Tickets</h1>
             <p>Selecteer hieronder uw bestemming, datum en aantal personen waarme u op reis wilt.</p>
+
+            <form>            
+                <Select options={airports} onChange={handleSelectAirport.bind(this)}></Select>
+                <h2>Vroegste datum heenreis:</h2>
+                <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                <h2>Laatste datum terugreis:</h2>
+                <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+                <h2>Kies de maximale reistijd:</h2>
+                <Select options={selectTimeOption} onChange={handleSelectAirport.bind(this)}></Select>           
+                
+                <label>
+                   <h2>Vul het aantal overnachtingen in:</h2>
+                   <input 
+                   type="number"
+                   required
+                   />
+               </label>
+               <div className="space">
+               <p>Ben je klaar met invullen? klik op de button hier onder.</p>
+               <p><button className='form-button'>Vind tickets</button></p>
+               </div>
+           </form>
 
 
         </div>
